2665e9b14b7f929f5e6c18c315c2a0bdfb0646a1
Daniel Desmond <df.dezmond9@gmail.com>
2021-06-18 14:54:36 -0400

    wrote code to generate full pug template

--- ch4/pug-project/pug-example.js
+++ ch4/pug-project/pug-example.js
@@ -1,5 +1,19 @@
 const pug = require("pug");
+const fs = require("fs");
 
-const template = "p Hello world";
+let data = {
+    title: "Practal Node.js",
+    author: {
+        twitter: "@azatmardan",
+        name: "Azat"
+    },
+    tags: ["express", "node", "javascript"]
+};
 
-pug.render(template, {pretty: true});
+data.body = process.argv[2];
+
+fs.readFile("pug-example.pug", "utf-8", (err, source) => {
+    let template = pug.compile(source);
+    let html = template(data);
+    console.log(html);
+});
